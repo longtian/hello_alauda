1331ed124fc34fec06a8963b928e15900db9097f
wangyan <wyvernnot@gmail.com>
2015-09-10 17:04:02 +0800

    body components backend

--- server.js
+++ server.js
@@ -2,15 +2,17 @@
  * Created by yan on 15-9-10.
  */
 
-try {
-    require('oneapm');
-} catch (e) {
-
-}
-
+var oneapm = require('oneapm');
 var express = require('express');
 var app = express();
 var path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
+var components = require(path.join(__dirname, 'public', 'components'));
 
+var handler = function (req, res) {
+    res.end('oks');
+};
+components.forEach(function (c) {
+    app.get('/' + c, handler);
+});
+app.use(express.static(path.join(__dirname, 'public')));
 app.listen(8888);
