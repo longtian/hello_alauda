f8b0b09ff38506714d5cec926388101e7fad0488
wangyan <wyvernnot@gmail.com>
2015-09-10 18:11:40 +0800

    server infomation

--- server.js
+++ server.js
@@ -2,17 +2,47 @@
  * Created by yan on 15-9-10.
  */
 
-var oneapm = require('oneapm');
+//var oneapm = require('oneapm');
 var express = require('express');
 var app = express();
 var path = require('path');
 var components = require(path.join(__dirname, 'public', 'components'));
+var os = require('os');
 
 var handler = function (req, res) {
     res.end('oks');
 };
+
 components.forEach(function (c) {
     app.get('/' + c, handler);
 });
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/README.md', function (req, res) {
+    require('fs')
+        .createReadStream(path.join(__dirname, 'README.md'))
+        .pipe(res);
+})
+
+app.get('/brief', function (req, res) {
+
+    var result = {};
+    result.version = process.version;
+    result.versions = process.versions;
+    result.arch = process.arch;
+    result.platform = process.platform;
+    result.pid = process.pid;
+    result.uptime = process.uptime();
+    result.hostname = os.hostname();
+    result.loadavg = os.loadavg();
+    result.networkInterfaces = os.networkInterfaces();
+    result.cpus = os.cpus();
+    result.release = os.release();
+
+    res.json(result);
+});
+
+
 app.listen(8888);
