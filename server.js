54ab819ad4adc04d359e82f3be47e5f7e1eb4ed7
wangyan <wyvernnot@gmail.com>
2015-09-11 17:17:35 +0800

    redis

--- server.js
+++ server.js
@@ -3,18 +3,32 @@
  */
 
 var oneapm = require('oneapm');
+var redis = require('redis');
+var client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
 var express = require('express');
 var app = express();
 var path = require('path');
 var components = require(path.join(__dirname, 'public', 'components'));
 var os = require('os');
 
-var handler = function (req, res) {
-    res.end('oks');
+var handler = function (name) {
+    return function (req, res) {
+        client.incr(name, function (err, count) {
+            if (err) {
+                console.error(arguments);
+                res.end();
+            } else {
+                res.json({
+                    name: name,
+                    count: count
+                });
+            }
+        });
+    };
 };
 
 components.forEach(function (c) {
-    app.get('/' + c, handler);
+    app.get('/' + c, handler(c));
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
